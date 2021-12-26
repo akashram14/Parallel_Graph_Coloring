51be4763f1491c28ed1c7d4fde94978c5b68bd7d
Akash <55029710+akashram14@users.noreply.github.com>
2021-12-26 18:11:10 +0530

    Update add.cu

--- add.cu
+++ add.cu
@@ -41,7 +41,7 @@ bool checkConflicts(vector<set<int>>& adj, int maxDegree, int n, int m, vector<i
 
 
 
-void readGraph(int &n, int &m, int &maxDegree, int** h_adj, int** h_adj_p){     //Take input
+void readGraph(int &n, int &m, int &maxDegree, int** h_degree, int** h_adj, int** h_adj_p){     //Take input
     int i, k, c;
     cout << "Enter the number of vertices : " << endl;
     cin >> n;
@@ -49,6 +49,7 @@ void readGraph(int &n, int &m, int &maxDegree, int** h_adj, int** h_adj_p){     
     cin >> m;
     if(m > (((long long)n * (n - 1))/ 2)){
         cout << "Invalid number of edges." << endl;
+        exit(0);
         return;
     }
 
@@ -100,6 +101,7 @@ void readGraph(int &n, int &m, int &maxDegree, int** h_adj, int** h_adj_p){     
 
     *h_adj_p = new int[n + 1];
     *h_adj = new int[(2 * m) + 1];
+    *h_degree = new int[n];
 
     int point = 0;
     for(i = 0;i < n; i++){
@@ -110,8 +112,10 @@ void readGraph(int &n, int &m, int &maxDegree, int** h_adj, int** h_adj_p){     
     (*h_adj_p)[n] = point;
 
     int mx = INT_MIN; 
-    for(i = 0;i < n; i++)                                                           //Find max degree
+    for(i = 0;i < n; i++){
+		(*h_degree)[i] = (int)adj[i].size();                                                         //Find max degree
         mx = max(mx, (int)adj[i].size());
+	}
     
     maxDegree = mx;
 
@@ -178,16 +182,15 @@ __global__ void colorSet(int* d_adj, int* d_adj_p, int* d_weights, bool* d_rem, 
     for(i = 1; i <= maxColours; i++){                                   //Assign color which is not there in neighbours' colors
         if(!forbidden[i]){
             d_colours[index] = i;
-            // printf("%d : %d\n", index, i);
             delete [] forbidden;
             return;
         }
     }
     delete [] forbidden;
 }
-//5 1 3 4 2 6
+
 int* colourGraph(int n, int m, int maxDegree, int* d_adj, int* d_adj_p, int* d_weights){
-    int i, rem_size = n, mx = min(n, 512);                                               //Initialize all variables
+    int i, rem_size = n, mx = min(n, 1024);                                               //Initialize all variables
     bool* h_rem = new bool[n];
     int* h_colours = new int[n];
     bool* d_rem;
@@ -196,10 +199,11 @@ int* colourGraph(int n, int m, int maxDegree, int* d_adj, int* d_adj_p, int* d_w
     cudaMalloc((void**)&d_colours, sizeof(int) * n);                                        //Allocate space on GPU
     cudaMalloc((void**)&d_rem, sizeof(bool) * n);
 	
-    initializeRemCol<<<mx, mx>>>(d_rem, d_colours, n);                                       
+	int blocks = (n + mx - 1)/mx;
+    initializeRemCol<<<blocks, mx>>>(d_rem, d_colours, n);                                       
 
     while(rem_size > 0){
-        colorSet<<<mx, mx>>>(d_adj, d_adj_p, d_weights, d_rem, d_colours, n, maxDegree);    //Launch kernel
+        colorSet<<<blocks, mx>>>(d_adj, d_adj_p, d_weights, d_rem, d_colours, n, maxDegree);    //Launch kernel
         cudaMemcpy(h_rem, d_rem, sizeof(bool) * n, cudaMemcpyDeviceToHost);                 //Copy back the updated uncolored set
         int k = 0;
         for(i = 0; i < n; i++){
@@ -217,6 +221,82 @@ int* colourGraph(int n, int m, int maxDegree, int* d_adj, int* d_adj_p, int* d_w
     return h_colours;
 }
 
+
+__global__ void colorSet2(int* d_adj, int* d_adj_p, int* d_weights, bool* d_rem, int* d_colours, int n, int maxDegree, int* d_degree){//pass h_deg here
+    int index = (blockDim.x * blockIdx.x) + threadIdx.x;    
+    if(index >= n)
+        return;
+    
+    if(!d_rem[index])                                                   //If current vertex is already colored, return
+        return;
+
+    int i, j, maxColours = maxDegree + 1;
+
+    for(i = d_adj_p[index]; i < d_adj_p[index + 1]; i++){               //Check if any uncolored neighbour has higher weight        
+        j = d_adj[i];                                                                   
+        if(d_rem[j] && (d_degree[index]<d_degree[j]  || (d_degree[index]==d_degree[j] && d_weights[j] > d_weights[index])) )  
+            return;
+    }
+
+    d_rem[index] = false;                                               //Current vertex should be coloured so remove it from remaining set
+
+    bool* forbidden = new bool[maxColours + 1];
+    for(i = 0; i < maxColours + 1; i++)
+        forbidden[i] = false;
+
+    for(i = d_adj_p[index]; i < d_adj_p[index + 1]; i++){               //Find out neighbours colors
+        j = d_adj[i];
+        forbidden[d_colours[j]] = true;
+    }
+    for(i = 1; i <= maxColours; i++){                                   //Assign color which is not there in neighbours' colors
+        if(!forbidden[i]){
+            d_colours[index] = i;
+            delete [] forbidden;
+            return;
+        }
+    }
+    delete [] forbidden;
+}
+
+int* colourGraph2(int n, int m, int maxDegree, int* d_adj, int* d_adj_p, int* d_weights, int* h_degree){
+    int i, rem_size = n, mx = min(n, 1024);                                               //Initialize all variables
+    bool* h_rem = new bool[n];
+    bool* h_inc = new bool[n];
+    int* h_colours = new int[n];
+    bool* d_rem;
+    int *d_colours;
+    int *d_degree;
+
+    cudaMalloc((void**)&d_degree, sizeof(int) * n); 
+    cudaMalloc((void**)&d_colours, sizeof(int) * n);                                        //Allocate space on GPU
+    cudaMalloc((void**)&d_rem, sizeof(bool) * n);
+
+    cudaMemcpy(d_degree, h_degree, sizeof(int) * n, cudaMemcpyHostToDevice);
+	
+	int blocks = (n + mx - 1)/mx;
+    initializeRemCol<<<blocks, mx>>>(d_rem, d_colours, n);                                       
+
+    while(rem_size > 0){
+        // cout << rem_size << endl;
+        colorSet2<<<blocks, mx>>>(d_adj, d_adj_p, d_weights, d_rem, d_colours, n, maxDegree, d_degree);    //Launch kernel
+        cudaMemcpy(h_rem, d_rem, sizeof(bool) * n, cudaMemcpyDeviceToHost);                 //Copy back the updated uncolored set
+        int k = 0;
+        for(i = 0; i < n; i++){
+            if(h_rem[i])
+                k++;
+        }
+        rem_size = k;
+    }
+
+    cudaMemcpy(h_colours, d_colours, sizeof(int) * n, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_colours);                                                                    //Free memory
+    cudaFree(d_rem);
+    cudaFree(d_degree);
+
+    return h_colours;
+}
+
 void randomWeightAssign(int* h_weights, int n){                                             //Assign random weights to the vertices
     vector<int> arr(n);
     for(int i=0; i<n; i++) arr[i] = i;
@@ -225,7 +305,6 @@ void randomWeightAssign(int* h_weights, int n){                                 
     for(int i=0; i<n; i++)
         h_weights[i] = arr[i];
 }
-
 
 int main(){
     int n, m, maxDegree;
@@ -236,8 +315,9 @@ int main(){
 
     int* h_adj = NULL, *h_adj_p = NULL;
     int* d_adj, *d_adj_p;
+    int* h_degree = NULL;
 
-    readGraph(n, m, maxDegree, &h_adj, &h_adj_p);                                       //Take input
+    readGraph(n, m, maxDegree, &h_degree, &h_adj, &h_adj_p);                                       //Take input
     int* h_weights = new int[n];
     int* d_weights;
 
@@ -245,9 +325,8 @@ int main(){
     cudaMalloc((void**)&d_adj_p, sizeof(int) * (n + 1));
     cudaMalloc((void**)&d_weights, sizeof(int) * n);
 
-    printf("Going in\n");
     randomWeightAssign(h_weights, n);
-    printf("out\n");
+
     cudaMemcpy(d_adj, h_adj, sizeof(int) * ((2 * m) + 1), cudaMemcpyHostToDevice);      //Copy data to GPU
     cudaMemcpy(d_adj_p, h_adj_p, sizeof(int) * (n + 1), cudaMemcpyHostToDevice);
     cudaMemcpy(d_weights, h_weights, sizeof(int) * n, cudaMemcpyHostToDevice);
@@ -274,7 +353,29 @@ int main(){
         min_colors = max(min_colors, colouring[i]);
 
     printf("The minimum colors needed => %d\n", min_colors);
-    printf("Parallel Graph coloring takes => %f ms\n",milliseconds);
+    printf("Parallel Graph coloring using Jones Plassmann algorithm takes => %f ms\n",milliseconds);
+    
+    
+    
+    
+    
+    cudaEventRecord(start);
+    
+    int *colouring2 = colourGraph2(n, m, maxDegree, d_adj, d_adj_p, d_weights, h_degree);
+    
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    
+    milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, start, stop);
+
+    min_colors = INT_MIN;
+    for(int i=0; i<n; i++)
+        min_colors = max(min_colors, colouring2[i]);
+
+    printf("The minimum colors needed => %d\n", min_colors);
+    printf("Parallel Graph coloring using Largest degree first takes => %f ms\n",milliseconds);
+    
 
     cudaFree(d_adj_p);
     cudaFree(d_adj);    
